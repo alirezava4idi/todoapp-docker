7584e69e317048a5e226217803755facecf34ff2
alirezava4idi <alireza.vahidi1@gmail.com>
2022-05-29 16:47:31 +0430

    added the mongo image & connected to DB

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require('./config/config');
 const PORT = process.env.PORT || 3000;
+
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+
+const connectWithRetry = () => {
+    mongoose.connect(mongoURL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(() => {
+        console.log("succesfully connected to DB");
+    }).catch((e) => {
+        console.log(e);
+        setTimeout(connectWithRetry, 5000);
+    });
+}
+connectWithRetry();
 
 app.get('/', (req, res) => {
     res.send("Welcome Home!");
